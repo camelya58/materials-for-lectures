fbd2c468ef5b2d40708fa3d318ac1b7ef9bd33ac
camelya58 <camelya@mail.ru>
2020-07-20 23:01:40 +0300

    add materials to lesson 5

---
+++ src/lesson5/operatorIf/If.java
@@ -0,0 +1,14 @@
+package lesson5.operatorIf;
+
+public class If {
+    public static void main(String[] args) {
+        // Case 1: if
+        int x = 10;
+
+        if (x <= 10) {
+            System.out.println("Это оператор if");
+        }
+        System.out.println("Этот текст будет выводиться всегда");
+
+    }
+}
---
+++ src/lesson5/operatorIf/IfElse.java
@@ -0,0 +1,13 @@
+package lesson5.operatorIf;
+
+public class IfElse {
+    public static void main(String[] args) {
+        // Case 2: if - else
+        int x = 10;
+        if (x > 20) {
+            System.out.println("Это оператор if");
+        } else {
+            System.out.println("Это оператор else");
+        }
+    }
+}
