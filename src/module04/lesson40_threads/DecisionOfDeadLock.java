87c20497297dea2667ebc040e2b527380cafde35
camelya58 <camelya@mail.ru>
2020-12-01 22:08:37 +0300

    add materials to lesson40 - threads

---
+++ src/module04/lesson40_threads/DecisionOfDeadLock.java
@@ -0,0 +1,29 @@
+package module04.lesson40_threads;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Класс DecisionOfDeadLock
+ *
+ * @author Kamila Meshcheryakova
+ * created 27.11.2020
+ */
+public class DecisionOfDeadLock {
+
+    public void twoLocks(Lock A, Lock B) {
+        while(true) {
+            if (A.tryLock()) {
+                if (B.tryLock()) {
+                    try {
+                        // do something
+                    } finally {
+                        B.unlock();
+                        A.unlock();
+                    }
+                } else {
+                    A.unlock();
+                }
+            }
+        }
+    }
+}
