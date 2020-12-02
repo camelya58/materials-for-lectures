54bcdc3def83f413a34cbd50b2cccf4922b86ace
Kamila M <camelya@mail.ru>
2020-12-02 11:27:28 +0300

    add extra materials to lesson40_threads

---
+++ src/module04/lesson40_threads/wait_notify/ConsumerTask.java
@@ -0,0 +1,21 @@
+package module04.lesson40_threads.wait_notify;
+
+public class ConsumerTask implements Runnable {
+    private TransferObject transferObject;
+    protected volatile boolean stopped;
+
+    public ConsumerTask(TransferObject transferObject) {
+        this.transferObject = transferObject;
+        new Thread(this, "ConsumerTask").start();
+    }
+
+    public void run() {
+        while (!stopped) {
+            transferObject.get();
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
